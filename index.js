11881e3c82663c84b9c6eaa09904201a7cef2fdd
BuildTools <unconfigured@null.spigotmc.org>
2018-10-07 17:40:32 +1000

    Updated code re:const

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-let search = require('./lib/search');
-
-console.log('books');
-console.log( search('books') );
-console.log('train spotting');
-console.log( search('train spotting') );
-console.log('Spotting');
-console.log( search('Spotting') );
-console.log('Sowing');
-console.log( search('Sewing'));
-console.log('Travel');
-console.log( search('Travel'));
-console.log('Drawing');
-console.log( search('Drawing'));
+module.exports = require('./lib/search');
